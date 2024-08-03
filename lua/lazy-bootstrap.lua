ce55895580e8d0abdf450ad4a7b017d8283f7fc8
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-08-04 02:33:24 +0700

    bruh

--- lua/lazy-bootstrap.lua
+++ lua/lazy-bootstrap.lua
@@ -1,4 +1,3 @@
--- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.uv.fs_stat(lazypath) then
@@ -9,5 +8,3 @@ if not vim.uv.fs_stat(lazypath) then
   end
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
-
--- vim: ts=2 sts=2 sw=2 et
