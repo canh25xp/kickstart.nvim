89779b834d68471eb1734b4a00f8621c550bf0af
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-08-04 04:04:27 +0700

    remove tokyonight

--- init.lua
+++ init.lua
@@ -15,7 +15,7 @@ require("config/autocmds")
 
 require("config/lazy")
 
-vim.cmd.colorscheme("catppuccin")
+vim.cmd.colorscheme("catppuccin-mocha")
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
