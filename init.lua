a6aba1148e94e2408a3cb940d70a57db508d9f4d
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-08-04 04:00:05 +0700

    change default theme

--- init.lua
+++ init.lua
@@ -15,5 +15,7 @@ require("config/autocmds")
 
 require("config/lazy")
 
+vim.cmd.colorscheme("catppuccin")
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
