86a035e1aa05fec2ba3af78be5473751bf9f90f2
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-08-04 16:17:24 +0700

    remove comments

--- init.lua
+++ init.lua
@@ -1,11 +1,8 @@
--- Set <space> as the leader key
--- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
--- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = true
+vim.g.have_nerd_font = false
 
 require("config/options")
 
@@ -17,5 +14,5 @@ require("config/lazy")
 
 vim.cmd.colorscheme("catppuccin-mocha")
 
--- The line beneath this is called `modeline`. See `:help modeline`
+-- See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
