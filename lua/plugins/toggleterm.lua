a747b127370cb7974f51b33675953154556963a6
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-08-04 15:14:46 +0700

    config toggleterm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,5 +1,20 @@
 return {
   "akinsho/toggleterm.nvim",
   version = "*",
-  config = true,
+  opts = {
+    size = 20,
+    open_mapping = [[<C-\>]],
+    hide_numbers = true,
+    shade_terminals = false,
+    start_in_insert = true,
+    insert_mappings = true,
+    persist_size = true,
+    close_on_exit = true,
+  },
+  cmd = "ToggleTerm",
+  keys = {
+    { [[<C-\>]] },
+    { "<leader>2", "<Cmd>2ToggleTerm<Cr>", desc = "Terminal #2" },
+    { "<leader>t", "<Cmd>ToggleTerm direction=tab<Cr>", desc = "Terminal in new tab" },
+  },
 }
