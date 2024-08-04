cab8f80f53e1efc868a2f3bc3db4d3f5ccb02e8c
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-08-05 00:19:18 +0700

    config dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -11,6 +11,10 @@ local logo = [[
 
 logo = string.rep("\n", 8) .. logo .. "\n\n"
 
+local ascii = require("ascii")
+
+local dynamic_logo = ascii.get_random("text", "neovim")
+
 return {
   "nvimdev/dashboard-nvim",
   lazy = false, -- As https://github.com/nvimdev/dashboard-nvim/pull/450, dashboard-nvim shouldn't be lazy-loaded to properly handle stdin.
@@ -18,21 +22,22 @@ return {
     local opts = {
       theme = "doom",
       hide = {
-        -- this is taken care of by lualine, enabling this messes up the actual laststatus setting after loading a file
-        statusline = false,
+        statusline = false, -- this is taken care of by lualine, enabling this messes up the actual laststatus setting after loading a file
       },
       config = {
-        header = require("ascii").get_random_global(),
+        header = ascii.art.text.doom.DooM,
         -- header = vim.split(logo, "\n"),
-          -- stylua: ignore
-          center = {
-            { action = 'lua LazyVim.pick()()',                                desc = " Find File",       icon = " ", key = "f" },
-            { action = "ene | startinsert",                                   desc = " New File",        icon = " ", key = "n" },
-            { action = 'lua require("persistence").load()',                   desc = " Restore Session", icon = " ", key = "s" },
-            { action = "Lazy",                                                desc = " Lazy",            icon = "󰒲 ", key = "l" },
-            { action = function() vim.api.nvim_input("<cmd>LazyGit<cr>") end, desc = " Lazygit",         icon = " ", key = "g" },
-            { action = function() vim.api.nvim_input("<cmd>qa<cr>") end,      desc = " Quit",            icon = " ", key = "q" },
-          },
+
+        -- stylua: ignore
+        center = {
+          { action = "Telescope find_files",                                desc = " Find File",       icon = " ", key = "f" },
+          { action = "ene | startinsert",                                   desc = " New File",        icon = " ", key = "n" },
+          { action = "Neotree reveal",                                      desc = " Neotree",         icon = " ", key = "e" },
+          { action = 'lua require("persistence").load()',                   desc = " Restore Session", icon = " ", key = "s" },
+          { action = "Lazy",                                                desc = " Lazy",            icon = "󰒲 ", key = "l" },
+          { action = function() vim.api.nvim_input("<cmd>LazyGit<cr>") end, desc = " Lazygit",         icon = " ", key = "g" },
+          { action = function() vim.api.nvim_input("<cmd>qa<cr>") end,      desc = " Quit",            icon = " ", key = "q" },
+        },
       },
     }
 
