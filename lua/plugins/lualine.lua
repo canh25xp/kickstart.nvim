bbeeda4e3e27c2ef6499832d40c62542b823bd45
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-08-04 17:28:54 +0700

    config lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -34,7 +34,7 @@ return {
   opts = {
     options = {
       theme = catppuccin,
-      component_separators = { left = "", right = "" },
+      component_separators = { left = "|", right = "|" },
       section_separators = { left = "", right = "" },
     },
     sections = {
