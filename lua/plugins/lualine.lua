58a34eb6191add21374a3ea9ae060a1f79082270
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-08-04 17:11:52 +0700

    replace mini statusline with lualine

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,48 @@
+-- stylua: ignore
+local colors = {
+  blue   = '#89b4fa',
+  cyan   = '#94e2d5',
+  black  = '#45475a',
+  white  = '#eff1f5',
+  red    = '#f38ba8',
+  violet = '#cba6f7',
+  grey   = '#303446',
+}
+
+local catppuccin = {
+  normal = {
+    a = { fg = colors.black, bg = colors.violet },
+    b = { fg = colors.white, bg = colors.grey },
+    c = { fg = colors.white },
+  },
+
+  insert = {
+    a = { fg = colors.black, bg = colors.blue },
+  },
+  visual = { a = { fg = colors.black, bg = colors.cyan } },
+  replace = { a = { fg = colors.black, bg = colors.red } },
+
+  inactive = {
+    a = { fg = colors.white, bg = colors.black },
+    b = { fg = colors.white, bg = colors.black },
+    c = { fg = colors.white },
+  },
+}
+
+return {
+  "nvim-lualine/lualine.nvim",
+  opts = {
+    options = {
+      theme = catppuccin,
+      component_separators = { left = "", right = "" },
+      section_separators = { left = "", right = "" },
+    },
+    sections = {
+      lualine_z = {
+        { "encoding" },
+        { "fileformat" },
+      },
+    },
+    extensions = { "neo-tree", "lazy", "toggleterm" },
+  },
+}
