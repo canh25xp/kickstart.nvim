63f651e8b77481c6d0bbd180e0ab99d2e17482c9
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-08-08 03:32:18 +0700

    bruh

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -2,6 +2,12 @@ return {
   "folke/noice.nvim",
   enabled = true,
   event = "VeryLazy",
+  dependencies = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    "MunifTanjim/nui.nvim",
+    -- `nvim-notify` is only needed, if you want to use the notification view. If not available, we use `mini` as the fallback
+    -- "rcarriga/nvim-notify",
+  },
   opts = {
     lsp = {
       override = {
@@ -46,10 +52,4 @@ return {
     end
     require("noice").setup(opts)
   end,
-  dependencies = {
-    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-    "MunifTanjim/nui.nvim",
-    -- `nvim-notify` is only needed, if you want to use the notification view. If not available, we use `mini` as the fallback
-    -- "rcarriga/nvim-notify",
-  },
 }
