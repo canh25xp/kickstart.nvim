c76f611526ed28cbfdc5229a955eb17b8f08b709
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-08-08 03:13:44 +0700

    add comments

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -30,10 +30,11 @@ return {
       view = "cmdline",
     },
     presets = {
-      command_palette = true,
-      lsp_doc_border = true,
-      bottom_search = true,
-      long_message_to_split = true,
+      bottom_search = true, -- use a classic bottom cmdline for search
+      command_palette = false, -- position the cmdline and popupmenu together
+      long_message_to_split = true, -- long messages will be sent to a split
+      inc_rename = false, -- enables an input dialog for inc-rename.nvim
+      lsp_doc_border = true, -- add a border to hover docs and signature help
     },
   },
   config = function(_, opts)
@@ -47,7 +48,7 @@ return {
   end,
   dependencies = {
     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-    -- "MunifTanjim/nui.nvim",
+    "MunifTanjim/nui.nvim",
     -- `nvim-notify` is only needed, if you want to use the notification view. If not available, we use `mini` as the fallback
     -- "rcarriga/nvim-notify",
   },
