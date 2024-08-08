5ef58f4e82a1aac0b28c4bf2c98346840934809a
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-08-08 22:03:56 +0700

    add autocmd

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,9 +1,31 @@
 --  See `:help lua-guide-autocommands`
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+local utils = require("config/utils")
+
+-- General Settings
+local general = augroup("General Settings", { clear = true })
 
 vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yanking (copying) text",
   group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
   callback = function()
     vim.highlight.on_yank()
+  end,
+})
+
+autocmd("FileType", {
+  group = general,
+  pattern = {
+    "help",
+    "checkhealth",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set("n", "q", "<cmd>close<cr>", {
+      buffer = event.buf,
+      silent = true,
+      desc = "Quit buffer",
+    })
   end,
 })
