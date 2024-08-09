d64ccd5083b72895544105e9efd88cf99826a4a9
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-08-09 21:30:23 +0700

    config keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -77,10 +77,10 @@ map("n", "[w", utils.DiagnosticGoto(false, "WARN"), { desc = "Prev Warning" })
 map("n", "<leader><tab>l", "<cmd>tablast<cr>", { desc = "Last Tab" })
 map("n", "<leader><tab>o", "<cmd>tabonly<cr>", { desc = "Close Other Tabs" })
 map("n", "<leader><tab>f", "<cmd>tabfirst<cr>", { desc = "First Tab" })
-map("n", "<leader><tab><tab>", "<cmd>tabnew<cr>", { desc = "New Tab" })
+map("n", "<leader><tab>c", "<cmd>tabnew<cr>", { desc = "Create Tab" })
 map("n", "<leader><tab>n", "<cmd>tabnext<cr>", { desc = "Next Tab" })
-map("n", "<C-_>", "<cmd>tabnext<cr>", { desc = "Next Tab" })
-map("n", "<leader><tab>d", "<cmd>tabclose<cr>", { desc = "Close Tab" })
+map({ "n", "t" }, "<leader><tab><tab>", "<cmd>tabnext<cr>", { desc = "Next Tab" })
+map("n", "<leader><tab>d", "<cmd>tabclose<cr>", { desc = "Delete Tab" })
 map("n", "<leader><tab>p", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
 
 -- lazy
