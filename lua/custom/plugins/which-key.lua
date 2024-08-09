896f13f77ac090c0d7478a623ae2a582dff61ad4
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-08-09 22:15:27 +0700

    config whichkey

--- lua/custom/plugins/which-key.lua
+++ lua/custom/plugins/which-key.lua
@@ -1,20 +1,43 @@
 return {
   "folke/which-key.nvim",
-  event = "VimEnter", -- Sets the loading event to 'VimEnter'
-  config = function() -- This is the function that runs, AFTER loading
-    require("which-key").setup()
-
-    -- Document existing key chains
-    require("which-key").add({
-      { "<leader>c", group = "[C]ode" },
-      { "<leader>d", group = "[D]ocument" },
-      { "<leader>r", group = "[R]ename" },
-      { "<leader>s", group = "[S]earch" },
-      { "<leader>w", group = "[W]orkspace" },
-      { "<leader>t", group = "[T]oggle" },
-      { "<leader>h", group = "Git [H]unk", mode = { "n", "v" } },
-    })
-  end,
+  event = "VeryLazy",
+  opts = {
+    preset = "classic",
+    spec = {
+      mode = { "n", "v" },
+      { "<leader><tab>", group = "tabs" },
+      { "<leader>c", group = "code" },
+      { "<leader>f", group = "file/find" },
+      { "<leader>g", group = "git" },
+      { "<leader>gh", group = "hunks" },
+      { "<leader>q", group = "quit/session" },
+      { "<leader>s", group = "search" },
+      { "<leader>u", group = "ui", icon = { icon = "󰙵 ", color = "cyan" } },
+      { "<leader>x", group = "diagnostics/quickfix", icon = { icon = "󱖫 ", color = "green" } },
+      { "[", group = "prev" },
+      { "]", group = "next" },
+      { "g", group = "goto" },
+      { "gs", group = "surround" },
+      { "z", group = "fold" },
+      {
+        "<leader>b",
+        group = "buffer",
+        expand = function()
+          return require("which-key.extras").expand.buf()
+        end,
+      },
+      {
+        "<leader>w",
+        group = "windows",
+        proxy = "<c-w>",
+        expand = function()
+          return require("which-key.extras").expand.win()
+        end,
+      },
+      -- better descriptions
+      { "gx", desc = "Open with system app" },
+    },
+  },
   keys = {
     {
       "<leader>?",
