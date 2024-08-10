9c4a9be81cd65469b1d975edc6d72583b1f52cd1
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-08-10 14:41:16 +0700

    config options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -16,35 +16,19 @@ vim.opt.softtabstop = 2
 vim.opt.confirm = true -- Ask to save changes before exiting modified buffer
 vim.opt.laststatus = 3 -- global statusline
 vim.opt.mousemoveevent = true
-
--- Don't show the mode, since it's already in the status line
-vim.opt.showmode = false
-
--- Sync clipboard between OS and Neovim.
--- Schedule the setting after `UiEnter` because it can increase startup-time.
--- Remove this option if you want your OS clipboard to remain independent.
---  :help 'clipboard'
-vim.schedule(function()
-  vim.opt.clipboard = "unnamedplus"
-end)
-
+vim.opt.virtualedit = "block" -- Allow cursor to move where there is no text in visual block mode
+vim.opt.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time
+vim.opt.showmode = false -- Don't show the mode, since it's already in the status line
+vim.opt.clipboard = "unnamedplus" -- Sync clipboard between OS and Neovim.
 vim.opt.breakindent = true
-
--- Save undo history
-vim.opt.undofile = true
-
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.opt.ignorecase = true
+vim.opt.undofile = true -- Save undo history
+vim.opt.ignorecase = true -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.opt.smartcase = true
-
--- Keep signcolumn on by default
-vim.opt.signcolumn = "yes"
-
--- Decrease update time
-vim.opt.updatetime = 250
-
--- Decrease mapped sequence wait time
-vim.opt.timeoutlen = 500
+vim.opt.updatetime = 250 -- Decrease update time
+vim.opt.timeoutlen = 500 -- Decrease mapped sequence wait time
+vim.opt.inccommand = "split" -- Preview substitutions live, as you type
+vim.opt.cursorline = true -- Show which line your cursor is on
+vim.opt.scrolloff = 4 -- Minimal number of screen lines to keep above and below the cursor.
 
 -- Configure how new splits should be opened
 vim.opt.splitright = true
@@ -52,13 +36,13 @@ vim.opt.splitbelow = true
 
 -- Sets how neovim will display certain whitespace characters in the editor.
 vim.opt.list = true
-
 vim.opt.listchars = {
   tab = "» ",
   trail = "·",
   nbsp = "␣",
 }
 
+-- Use a bold seperators
 vim.opt.fillchars = {
   horiz = "━",
   horizup = "┻",
@@ -69,28 +53,18 @@ vim.opt.fillchars = {
   verthoriz = "╋",
 }
 
--- Preview substitutions live, as you type!
-vim.opt.inccommand = "split"
-
--- Show which line your cursor is on
-vim.opt.cursorline = true
-
--- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 4
-
 if vim.g.neovide then
   -- vim.o.guifont = "CaskaydiaCove Nerd Font:h12"
   vim.g.neovide_fullscreen = false
   vim.g.neovide_scroll_animation_length = 0.3
   vim.g.neovide_cursor_vfx_mode = "sonicboom"
   vim.g.neovide_hide_mouse_when_typing = true
-  -- vim.g.neovide_transparency = 0.8
+  vim.g.neovide_transparency = 1.0
 end
 
 if not vim.fn.has("win32") or not vim.fn.has("win64") then
   vim.opt.shell = "pwsh"
-  vim.opt.shellcmdflag =
-    "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+  vim.opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
   vim.opt.shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait"
   vim.opt.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
   vim.opt.shellquote = ""
