1c68302f1f4231d22c800d5ec8e6ebf1c95fa2e0
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-08-10 15:12:09 +0700

    add keyboard

--- lua/plugins/extras/ascii.lua
+++ lua/plugins/extras/ascii.lua
@@ -2,6 +2,15 @@ return {
   "MaximilianLloyd/ascii.nvim",
   enabled = true,
   lazy = true,
+  keys = {
+    {
+      "<leader>a",
+      function()
+        require("ascii").preview()
+      end,
+      desc = "Ascii Preview",
+    },
+  },
   dependencies = {
     "MunifTanjim/nui.nvim",
   },
