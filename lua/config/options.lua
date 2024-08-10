329e47f6a9b48a6a3321e6eb54956d691eaf0a51
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-08-10 23:12:42 +0700

    config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,6 +3,7 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 vim.g.have_nerd_font = true
 
+vim.opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp", "folds" }
 vim.opt.termguicolors = true -- True color support
 vim.opt.number = true
 vim.opt.relativenumber = true
