383674b71a5586708d5ea63716299718109c37ae
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-08-10 13:38:41 +0700

    add init file

---
+++ lua/config/init.lua
@@ -0,0 +1,9 @@
+require("config.options")
+require("config.keymaps")
+require("config.autocmds")
+require("config.lazy")
+
+vim.cmd.colorscheme("catppuccin-mocha")
+
+-- See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
