101220067bc20d02f26df6dd59fdf7237d9daa43
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-08-12 00:56:04 +0700

    shortmess:append

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -25,7 +25,7 @@ vim.opt.linebreak = true -- Wrap lines at convenient points
 vim.opt.pumblend = 10 -- Popup blend
 vim.opt.pumheight = 10 -- Maximum number of entries in a popup
 vim.opt.pumwidth = 15 -- Minimum width for the popup menu
--- vim.opt.shortmess:append({ W = true, c = true, C = true })
+vim.opt.shortmess:append({ W = true, c = true, C = true })
 vim.opt.wrap = false
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
@@ -81,7 +81,9 @@ vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSi
 vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
 vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
 
-vim.cmd.colorscheme("sorbet")
+if vim.g.minimal_config then
+  vim.cmd.colorscheme("sorbet")
+end
 
 if vim.g.neovide then
   -- vim.o.guifont = "CaskaydiaCove Nerd Font:h12"
