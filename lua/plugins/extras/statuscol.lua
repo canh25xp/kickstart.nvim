547f043e7140ca77ae737542e1a02bfc0ee5fc64
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-08-11 17:35:36 +0700

    .

--- lua/plugins/extras/statuscol.lua
+++ lua/plugins/extras/statuscol.lua
@@ -5,33 +5,34 @@ return {
     local builtin = require("statuscol.builtin")
     require("statuscol").setup({
       segments = {
-        -- { text = { "%C" }, click = "v:lua.ScFa" },
         {
-          sign = { namespace = { "diagnostic/signs" }, maxwidth = 1, auto = false },
+          sign = { namespace = { "diagnostic/signs" }, maxwidth = 1, auto = true },
           click = "v:lua.ScSa",
         },
         {
-          text = { builtin.lnumfunc, "│ " },
+          sign = { namespace = { "gitsigns" }, maxwidth = 1, auto = true },
+          click = "v:lua.ScSa",
+        },
+        {
+          text = { builtin.lnumfunc, "│" },
           condition = { true, builtin.not_empty },
           click = "v:lua.ScLa",
         },
-        {
-          sign = { namespace = { "gitsigns" }, maxwidth = 1, auto = false },
-          click = "v:lua.ScSa",
-        },
       },
       -- segments = {
-      --   { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+      --   {
+      --     text = { builtin.foldfunc },
+      --     click = "v:lua.ScFa",
+      --   },
       --   {
       --     sign = { namespace = { "diagnostic/signs" }, maxwidth = 2, auto = true },
-      --     click = "v:lua.ScSa"
+      --     click = "v:lua.ScSa",
       --   },
-      --   { text = { builtin.lnumfunc }, click = "v:lua.ScLa", },
       --   {
       --     sign = { name = { ".*" }, maxwidth = 2, colwidth = 1, auto = true, wrap = true },
-      --     click = "v:lua.ScSa"
+      --     click = "v:lua.ScSa",
       --   },
-      -- }
+      -- },
     })
   end,
 }
