b72a5e03e6c5e1491d2389a1b1672fa6b6a08cbd
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-08-13 20:47:52 +0700

    define icon

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -65,21 +65,35 @@ vim.opt.listchars = {
   nbsp = "␣",
 }
 
--- Use a bold seperators
-vim.opt.fillchars = {
-  horiz = "━",
-  horizup = "┻",
-  horizdown = "┳",
-  vert = "┃",
-  vertleft = "┫",
-  vertright = "┣",
-  verthoriz = "╋",
-}
+if vim.g.have_nerd_font then
+  -- Use a bold seperators
+  -- stylua: ignore
+  vim.opt.fillchars = {
+    horiz     = "━",
+    horizup   = "┻",
+    horizdown = "┳",
+    vert      = "┃",
+    vertleft  = "┫",
+    vertright = "┣",
+    verthoriz = "╋",
+  }
 
-vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
-vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
-vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
-vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
+  vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
+  vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
+  vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
+  vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
+else
+  -- stylua: ignore
+  vim.opt.fillchars = {
+    horiz     = "-",
+    horizup   = "-",
+    horizdown = "-",
+    vert      = "|",
+    vertleft  = "|",
+    vertright = "|",
+    verthoriz = "+",
+  }
+end
 
 vim.cmd.colorscheme("sorbet")
 
