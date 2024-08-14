ae61335cbd86f15cdebef6ea6fa3624b50c922d1
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-08-15 02:55:45 +0700

    config fidget

--- lua/plugins/fidget.lua
+++ lua/plugins/fidget.lua
@@ -2,6 +2,19 @@ return {
   "j-hui/fidget.nvim",
   lazy = true,
   opts = {
+    notification = {
+      window = {
+        winblend = 50, -- Background color opacity in the notification window
+        border = "none", -- Border around the notification window
+        zindex = 45, -- Stacking priority of the notification window
+        max_width = 0, -- Maximum width of the notification window
+        max_height = 0, -- Maximum height of the notification window
+        x_padding = 0, -- Padding from right edge of window boundary
+        y_padding = 0, -- Padding from bottom edge of window boundary
+        align = "bottom", -- How to align the notification window
+        relative = "editor", -- What the notification window position is relative to
+      },
+    },
     progress = {
       display = {
         render_limit = 16, -- How many LSP messages to show at once
