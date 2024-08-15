4fa934c6b80bab38e84602bf599ad8356021f5c2
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-08-16 00:24:06 +0700

    fidget catppuccin integration

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -36,6 +36,7 @@ return {
       cmp = true,
       dashboard = true,
       flash = true,
+      fidget = true,
       grug_far = true,
       gitsigns = true,
       headlines = true,
--- lua/plugins/fidget.lua
+++ lua/plugins/fidget.lua
@@ -4,7 +4,7 @@ return {
   opts = {
     notification = {
       window = {
-        winblend = 50, -- Background color opacity in the notification window
+        winblend = 0, -- Background color opacity in the notification window
         border = "none", -- Border around the notification window
         zindex = 45, -- Stacking priority of the notification window
         max_width = 0, -- Maximum width of the notification window
