e95c1a6984088914b410e3f3b94e11acbe1b3b16
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-08-15 20:35:44 +0700

    .

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -14,31 +14,32 @@ return {
   cmd = "ToggleTerm",
   keys = {
     { [[<C-\>]] },
-    { "<leader>2", "<Cmd>2ToggleTerm<Cr>", desc = "Terminal #2" },
+    { "<leader>t1", "<Cmd>1ToggleTerm<Cr>", desc = "Terminal #2" },
+    { "<leader>t2", "<Cmd>2ToggleTerm<Cr>", desc = "Terminal #2" },
     { "<leader>tt", "<Cmd>ToggleTerm direction=tab<Cr>", desc = "ToggleTerm in new tab" },
-    {
-      "<leader>gg",
-      function()
-        local lazygit = require("toggleterm.terminal").Terminal:new({
-          cmd = "lazygit",
-          dir = "git_dir",
-          direction = "tab",
-        })
-        lazygit:toggle()
-      end,
-      desc = "Lazygit",
-    },
-    {
-      "<leader>gl",
-      function()
-        local lazygit = require("toggleterm.terminal").Terminal:new({
-          cmd = "lazygit log",
-          dir = "git_dir",
-          direction = "tab",
-        })
-        lazygit:toggle()
-      end,
-      desc = "Lazygit log",
-    },
+    -- {
+    --   "<leader>gg",
+    --   function()
+    --     local lazygit = require("toggleterm.terminal").Terminal:new({
+    --       cmd = "lazygit",
+    --       dir = "git_dir",
+    --       direction = "tab",
+    --     })
+    --     lazygit:toggle()
+    --   end,
+    --   desc = "Lazygit",
+    -- },
+    -- {
+    --   "<leader>gl",
+    --   function()
+    --     local lazygit = require("toggleterm.terminal").Terminal:new({
+    --       cmd = "lazygit log",
+    --       dir = "git_dir",
+    --       direction = "tab",
+    --     })
+    --     lazygit:toggle()
+    --   end,
+    --   desc = "Lazygit log",
+    -- },
   },
 }
