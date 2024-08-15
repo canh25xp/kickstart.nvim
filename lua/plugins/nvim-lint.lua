45f94554e660d3bb6485f25cb6a66664d92542e6
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-08-16 00:54:19 +0700

    change nvim-lint event

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -1,6 +1,7 @@
 return {
   "mfussenegger/nvim-lint",
-  event = { "BufReadPre", "BufNewFile" },
+  -- event = { "BufReadPre", "BufNewFile" },
+  events = { "BufWritePost", "BufReadPost", "InsertLeave" },
   config = function()
     local lint = require("lint")
     lint.linters_by_ft = {
