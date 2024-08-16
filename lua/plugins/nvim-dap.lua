a868968e9322753f6350b012624d2d30898bd439
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-08-16 23:54:49 +0700

    config nvim-dap

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -11,84 +11,100 @@ local function get_args(config)
 end
 
 return {
-  "mfussenegger/nvim-dap",
-  dependencies = {
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "rcarriga/nvim-dap-ui",
+      "williamboman/mason.nvim",
+      "jay-babu/mason-nvim-dap.nvim",
+    },
+    -- stylua: ignore
+    keys = {
+      { "<leader>d", "", desc = "+debug", mode = {"n", "v"} },
+      { "<F5>",       function() require("dap").continue() end, desc = "Debug: Start/Continue" },
+      { "<F10>",      function() require("dap").step_over() end, desc = "Debug: Step Over" },
+      { "<F11>",      function() require("dap").step_into() end, desc = "Debug: Step In" },
+      { "<F12>",      function() require("dap").step_out() end, desc = "Debug: Step Out" },
+      { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Debug: Toggle Breakpoint" },
+      { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: ")) end, desc = "Debug: Set Breakpoint", },
+      { "<leader>dc", function() require("dap").continue() end, desc = "Debug: Start/Continue" },
+      { "<leader>da", function() require("dap").continue({ before = get_args }) end, desc = "Debug: Run with Args" },
+      { "<leader>dC", function() require("dap").run_to_cursor() end, desc = "Debug: Run to Cursor" },
+      { "<leader>dg", function() require("dap").goto_() end, desc = "Debug: Go to Line (No Execute)" },
+      { "<leader>di", function() require("dap").step_into() end, desc = "Debug: Step Into" },
+      { "<leader>dj", function() require("dap").down() end, desc = "Down" },
+      { "<leader>dk", function() require("dap").up() end, desc = "Up" },
+      { "<leader>dl", function() require("dap").run_last() end, desc = "Run Last" },
+      { "<leader>do", function() require("dap").step_out() end, desc = "Debug: Step Out" },
+      { "<leader>dO", function() require("dap").step_over() end, desc = "Debug: Step Over" },
+      { "<leader>dp", function() require("dap").pause() end, desc = "Debug: Pause" },
+      { "<leader>dr", function() require("dap").repl.toggle() end, desc = "Debug: Toggle REPL" },
+      { "<leader>ds", function() require("dap").session() end, desc = "Debug: Session" },
+      { "<leader>dt", function() require("dap").terminate() end, desc = "Debug: Terminate" },
+      { "<leader>dw", function() require("dap.ui.widgets").hover() end, desc = "Debug: Widgets" },
+    },
+    config = function()
+      require("mason-nvim-dap").setup({
+        automatic_installation = true,
+        handlers = {},
+        ensure_installed = {
+          ensure_installed = {
+            "python",
+            "cppdbg",
+          },
+        },
+      })
+    end,
+  },
+  {
     "rcarriga/nvim-dap-ui",
-    "nvim-neotest/nvim-nio", -- Required dependency for nvim-dap-ui
-    "williamboman/mason.nvim",
-    "jay-babu/mason-nvim-dap.nvim",
-  },
-  -- stylua: ignore
-  keys = {
-    { "<leader>d", "", desc = "+debug", mode = {"n", "v"} },
-    { "<F5>",       function() require("dap").continue() end, desc = "Debug: Start/Continue" },
-    { "<F10>",      function() require("dap").step_over() end, desc = "Debug: Step Over" },
-    { "<F11>",      function() require("dap").step_into() end, desc = "Debug: Step In" },
-    { "<F12>",      function() require("dap").step_out() end, desc = "Debug: Step Out" },
-    { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Debug: Toggle Breakpoint" },
-    { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: ")) end, desc = "Debug: Set Breakpoint", },
-    { "<leader>dc", function() require("dap").continue() end, desc = "Debug: Start/Continue" },
-    { "<leader>da", function() require("dap").continue({ before = get_args }) end, desc = "Debug: Run with Args" },
-    { "<leader>dC", function() require("dap").run_to_cursor() end, desc = "Debug: Run to Cursor" },
-    { "<leader>dg", function() require("dap").goto_() end, desc = "Debug: Go to Line (No Execute)" },
-    { "<leader>di", function() require("dap").step_into() end, desc = "Debug: Step Into" },
-    { "<leader>dj", function() require("dap").down() end, desc = "Down" },
-    { "<leader>dk", function() require("dap").up() end, desc = "Up" },
-    { "<leader>dl", function() require("dap").run_last() end, desc = "Run Last" },
-    { "<leader>do", function() require("dap").step_out() end, desc = "Debug: Step Out" },
-    { "<leader>dO", function() require("dap").step_over() end, desc = "Debug: Step Over" },
-    { "<leader>dp", function() require("dap").pause() end, desc = "Debug: Pause" },
-    { "<leader>dr", function() require("dap").repl.toggle() end, desc = "Debug: Toggle REPL" },
-    { "<leader>ds", function() require("dap").session() end, desc = "Debug: Session" },
-    { "<leader>dt", function() require("dap").terminate() end, desc = "Debug: Terminate" },
-    { "<leader>dw", function() require("dap.ui.widgets").hover() end, desc = "Debug: Widgets" },
-    { "<leader>du", function() require("dapui").toggle() end, desc = "Debug: Toggle Dapui" },
-  },
-  config = function()
-    local dap = require("dap")
-    local dapui = require("dapui")
-
-    require("mason-nvim-dap").setup({
-      -- Makes a best effort to setup the various debuggers with
-      -- reasonable debug configurations
-      automatic_installation = true,
-
-      -- You can provide additional configuration to the handlers,
-      -- see mason-nvim-dap README for more information
-      handlers = {},
-
-      -- You'll need to check that you have the required things installed
-      -- online, please don't ask me how to install them :)
-      ensure_installed = {
-        "python",
-        "cppdbg",
-      },
-    })
-
-    -- Dap UI setup
-    -- For more information, see |:help nvim-dap-ui|
-    dapui.setup({
-      -- Set icons to characters that are more likely to work in every terminal.
-      --    Feel free to remove or use ones that you like more! :)
-      --    Don't feel like these are good choices.
-      icons = { expanded = "▾", collapsed = "▸", current_frame = "*" },
-      controls = {
-        icons = {
-          pause = "⏸",
-          play = "▶",
-          step_into = "⏎",
-          step_over = "⏭",
-          step_out = "⏮",
-          step_back = "b",
-          run_last = "▶▶",
-          terminate = "⏹",
-          disconnect = "⏏",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      -- "theHamsta/nvim-dap-virtual-text",
+    },
+    -- stylua: ignore
+    keys = {
+      { "<leader>du", function() require("dapui").toggle({ }) end, desc = "Debug: Toggle Dap UI" },
+      { "<leader>de", function() require("dapui").eval() end, desc = "Debug: Eval", mode = {"n", "v"} },
+    },
+    opts = {
+      floating = {
+        border = "single",
+        mappings = {
+          close = { "q", "<Esc>" },
         },
       },
-    })
-
-    dap.listeners.after.event_initialized["dapui_config"] = dapui.open
-    dap.listeners.before.event_terminated["dapui_config"] = dapui.close
-    dap.listeners.before.event_exited["dapui_config"] = dapui.close
-  end,
+      icons = { expanded = "▾", collapsed = "▸", current_frame = "*" },
+      layouts = {
+        {
+          elements = {
+            { id = "scopes", size = 0.25 },
+            { id = "breakpoints", size = 0.25 },
+            { id = "stacks", size = 0.25 },
+            { id = "watches", size = 0.25 },
+          },
+          position = "left",
+          size = 40,
+        },
+        {
+          elements = {
+            { id = "console", size = 1.0 },
+            -- { id = "repl", size = 0.5 },
+          },
+          position = "bottom",
+          size = 10,
+        },
+      },
+    },
+    config = function(_, opts)
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup(opts)
+      -- stylua: ignore start
+      dap.listeners.after.event_initialized["dapui_config"] = function() dapui.open({}) end
+      dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close({}) end
+      dap.listeners.before.event_exited["dapui_config"] = function() dapui.close({}) end
+      -- stylua: ignore stop
+    end,
+  },
 }
