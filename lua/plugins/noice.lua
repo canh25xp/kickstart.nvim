45428d84161cbcd6b049f5dcad11502fc5f14ac6
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-08-17 13:10:19 +0700

    config noice

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -3,20 +3,27 @@ return {
   enabled = true,
   event = "VeryLazy",
   dependencies = {
-    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
     "MunifTanjim/nui.nvim",
-    -- `nvim-notify` is only needed, if you want to use the notification view. If not available, we use `mini` as the fallback
     -- "rcarriga/nvim-notify",
+  },
+  -- stylua: ignore
+  keys = {
+    { "<leader>sn", "", desc = "+noice"},
+    { "<S-Enter>", function() require("noice").redirect(vim.fn.getcmdline()) end, mode = "c", desc = "Redirect Cmdline" },
+    { "<leader>snl", function() require("noice").cmd("last") end, desc = "Noice Last Message" },
+    { "<leader>snh", function() require("noice").cmd("history") end, desc = "Noice History" },
+    { "<leader>sna", function() require("noice").cmd("all") end, desc = "Noice All" },
+    { "<leader>snd", function() require("noice").cmd("dismiss") end, desc = "Dismiss All" },
+    { "<leader>snt", function() require("noice").cmd("pick") end, desc = "Noice Picker (Telescope/FzfLua)" },
+    { "<c-f>", function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end, silent = true, expr = true, desc = "Scroll Forward", mode = {"i", "n", "s"} },
+    { "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true, desc = "Scroll Backward", mode = {"i", "n", "s"}},
   },
   opts = {
     lsp = {
       override = {
-        -- override the default lsp markdown formatter with Noice
-        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-        -- override the lsp markdown formatter with Noice
-        ["vim.lsp.util.stylize_markdown"] = true,
-        -- override cmp documentation with Noice (needs the other options to work)
-        ["cmp.entry.get_documentation"] = true,
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true, -- override the default lsp markdown formatter with Noice
+        ["vim.lsp.util.stylize_markdown"] = true, -- override cmp documentation with Noice (needs the other options to work)
+        ["cmp.entry.get_documentation"] = true, -- override the lsp markdown formatter with Noice
       },
     },
     routes = {
@@ -34,6 +41,11 @@ return {
     },
     cmdline = {
       view = "cmdline",
+    },
+    views = {
+      split = {
+        enter = true,
+      },
     },
     presets = {
       bottom_search = true, -- use a classic bottom cmdline for search
