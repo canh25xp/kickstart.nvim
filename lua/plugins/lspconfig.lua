b8189be43ab92e35e911ff83e150bef8c07a9d12
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-08-20 21:39:40 +0700

    add shfmt ensureinstalled

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -99,6 +99,7 @@ return {
       vim.list_extend(ensure_installed, {
         "stylua", -- Used to format Lua code
         "markdownlint",
+        "shfmt",
       })
       require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
