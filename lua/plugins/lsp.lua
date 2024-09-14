4d76e9e9f13a8200d903a19e4da1e87e9f11ccea
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-09-14 14:56:37 +0700

    add powershell lsp support

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -78,6 +78,7 @@ return {
       capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
       local servers = {
+        powershell_es = {},
         clangd = {},
         pyright = {},
         lua_ls = {
