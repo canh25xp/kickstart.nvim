13e90038664380ab5b1b1a5b605dc9f2694b4579
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-09-16 01:41:00 +0700

    append tools bin to vim path

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -8,3 +8,8 @@ end
 if vim.g.vscode then
   require("config.vscode")
 end
+
+local tools_bin_dir = vim.fn.stdpath("data") .. "/tools"
+local lua_language_server_bin_dir = tools_bin_dir .. "/lua-language-server/bin"
+
+vim.env.PATH = vim.env.PATH .. vim.g.path_sep .. lua_language_server_bin_dir
