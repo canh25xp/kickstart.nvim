df6dbbbaf3ee00469713436264f17e184b374509
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-09-18 07:27:44 +0700

    using luarc

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -26,6 +26,7 @@ return {
             vim.lsp.buf.format({ async = true })
           end, "Format")
         end
+
       end
 
       local capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -46,23 +47,8 @@ return {
         })
       end
 
-      -- Change border of documentation hover window, See https://github.com/neovim/neovim/pull/13998.
       utils.set_lsp_border("rounded")
       utils.signcolumn_single_sign()
     end,
-  },
-  {
-    "folke/lazydev.nvim",
-    ft = "lua",
-    opts = {
-      library = {
-        { path = "luvit-meta/library", words = { "vim%.uv" } },
-        { plugins = { "nvim-dap-ui" }, types = true },
-      },
-    },
-  },
-  {
-    "Bilal2453/luvit-meta",
-    lazy = true,
   },
 }
