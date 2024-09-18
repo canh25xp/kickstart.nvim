bb996185f00d38769fd8807e0c82416c3fa1c048
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-09-18 23:58:58 +0700

    enabled cmp windows border

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -23,6 +23,10 @@ return {
         end,
       },
       completion = { completeopt = "menu,menuone,noinsert" },
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
       mapping = cmp.mapping.preset.insert({
         ["<C-n>"] = cmp.mapping.select_next_item(),
         ["<C-p>"] = cmp.mapping.select_prev_item(),
