74b011dbad50c0bb8f3001319c35ebc742a9f909
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-09-21 00:52:10 +0700

    add toggle background transparency shortcut

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -16,10 +16,20 @@ return {
       end,
       desc = "Change Theme",
     },
+    {
+      "<leader>ub",
+      function()
+        local cat = require("catppuccin")
+        cat.options.transparent_background = not cat.options.transparent_background
+        cat.compile()
+        vim.cmd.colorscheme(vim.g.colors_name)
+      end,
+      desc = "Toggle Transparency",
+    },
   },
   opts = {
     flavour = "auto", -- latte, frappe, macchiato, mocha
-    transparent_background = false,
+    transparent_background = true,
     term_colors = true, -- sets terminal colors (e.g. `g:terminal_color_0`)
     background = {
       light = "latte",
