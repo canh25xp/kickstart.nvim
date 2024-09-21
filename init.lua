ce3415d39f5205cffb3a02028b7aa3e92003d74d
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-09-22 00:21:10 +0700

    edit remove local vimrc_path variable

--- init.lua
+++ init.lua
@@ -1,7 +1,6 @@
 vim.opt.runtimepath:prepend("~/.vim/after")
 vim.opt.packpath = vim.opt.runtimepath:get()
-local vimrc_path = vim.fn.expand("~/.vimrc")
-if vim.loop.fs_stat(vimrc_path) then
-  vim.cmd("source " .. vimrc_path)
+if vim.loop.fs_stat("~/.vimrc") then
+  vim.cmd("source ~/.vimrc")
 end
 require("config")
