1b0327fc1d8f5953781b4add4ec5e3f5718f2245
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-09-21 23:27:47 +0700

    check vimrc before source

--- init.lua
+++ init.lua
@@ -1,4 +1,7 @@
 vim.opt.runtimepath:prepend("~/.vim/after")
 vim.opt.packpath = vim.opt.runtimepath:get()
-vim.cmd("source ~/.vimrc")
+local vimrc_path = vim.fn.expand("~/.vimrc")
+if vim.loop.fs_stat(vimrc_path) then
+  vim.cmd("source " .. vimrc_path)
+end
 require("config")
