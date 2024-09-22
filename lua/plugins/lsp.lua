9dddeeb475210700b000bd1dff45242a30f15d6a
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-09-22 20:05:06 +0700

    autocmd: open diagnostic at current cursor

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,5 +1,6 @@
 local api = vim.api
 local lsp = vim.lsp
+local diagnostic = vim.diagnostic
 local utils = require("common.utils")
 
 return {
@@ -27,11 +28,36 @@ return {
         map("n", "<leader>cr", lsp.buf.rename, "Rename")
         map("n", "<leader>ca", lsp.buf.code_action, "Code Action")
 
+        -- Keymap for LSP format
         if client.server_capabilities.documentFormattingProvider then
           map({ "n", "x" }, "<leader>cF", function()
             lsp.buf.format({ async = true })
           end, "Format")
         end
+
+        api.nvim_create_autocmd("CursorHold", {
+          buffer = bufnr,
+          callback = function()
+            local float_opts = {
+              focusable = false,
+              close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+              border = "rounded",
+              source = "always", -- show source in diagnostic popup window
+              prefix = " ",
+            }
+
+            if not vim.b.diagnostics_pos then
+              vim.b.diagnostics_pos = { nil, nil }
+            end
+
+            local cursor_pos = api.nvim_win_get_cursor(0)
+            if (cursor_pos[1] ~= vim.b.diagnostics_pos[1] or cursor_pos[2] ~= vim.b.diagnostics_pos[2]) and #diagnostic.get() > 0 then
+              diagnostic.open_float(nil, float_opts)
+            end
+
+            vim.b.diagnostics_pos = cursor_pos
+          end,
+        })
 
         -- Highlight the current variable and its usages in the buffer.
         if client.server_capabilities.documentHighlightProvider then
