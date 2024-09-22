7f7b7f0607132f01c100ad20844d18c52164e1dd
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-09-22 17:06:02 +0700

    add luals install scripts

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -12,4 +12,4 @@ end
 local tools_bin_dir = vim.fn.stdpath("data") .. "/tools"
 local lua_language_server_bin_dir = tools_bin_dir .. "/lua-language-server/bin"
 
-vim.env.PATH = vim.env.PATH .. vim.g.path_sep .. lua_language_server_bin_dir
+vim.env.PATH = vim.env.PATH .. vim.g.path_sep .. lua_language_server_bin_dir .. vim.g.path_sep
