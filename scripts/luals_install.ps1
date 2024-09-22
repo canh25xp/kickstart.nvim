e2d320d928c7a9ebf8de460ffaf37bd2a1faccda
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-09-22 19:25:44 +0700

    edit install script

--- scripts/luals_install.ps1
+++ scripts/luals_install.ps1
@@ -1,21 +1,16 @@
 # Set policy to avoid errors
 #Set-ExecutionPolicy RemoteSigned -scope CurrentUser
 
-# Install lua-language-server
-$lua_ls_link = "https://github.com/LuaLS/lua-language-server/releases/download/3.10.6/lua-language-server-3.10.6-win32-x64.zip"
-$lua_ls_install_dir = "$env:LOCALAPPDATA\nvim-data\tools"
-$lua_ls_src_path = "$lua_ls_install_dir\lua-language-server.zip"
-$lua_ls_dir = "$lua_ls_install_dir\lua-language-server"
+$tools_dir = "$env:LOCALAPPDATA\nvim-data\tools"
+$download_link = "https://github.com/LuaLS/lua-language-server/releases/download/3.10.6/lua-language-server-3.10.6-win32-x64.zip"
+$download_path = "$tools_dir\lua-language-server.zip"
+$install_dir = "$tools_dir\lua-language-server"
 
-mkdir $lua_ls_install_dir
+curl -L $download_link -o $download_path
 
-# Download file, ref: https://stackoverflow.com/a/51225744/6064933
-Invoke-WebRequest $lua_ls_link -OutFile "$lua_ls_src_path"
+7z x "$download_path" -o"$install_dir"
 
-# Extract the zip file using 7zip, ref: https://stackoverflow.com/a/41933215/6064933
-7z x "$lua_ls_src_path" -o"$lua_ls_dir"
-
-rm $lua_ls_src_path
+Remove-Item $download_path
 
 # Setup PATH env variable, ref: https://stackoverflow.com/q/714877/6064933
 #[Environment]::SetEnvironmentVariable("Path", $env:Path + ";$lua_ls_dir\bin", "Machine")
