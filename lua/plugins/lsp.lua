87866bae0a886f57360ed68159d3b7886c14f105
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-09-25 00:19:58 +0700

    remap inlay hint toggle

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -63,7 +63,7 @@ return {
         require("clangd_extensions.inlay_hints").set_inlay_hints()
         local group = vim.api.nvim_create_augroup("clangd_no_inlay_hints_in_insert", { clear = true })
 
-        vim.keymap.set("n", "<leader>ch", function()
+        vim.keymap.set("n", "<leader>uh", function()
           if require("clangd_extensions.inlay_hints").toggle_inlay_hints() then
             vim.api.nvim_create_autocmd("InsertEnter", { group = group, buffer = bufnr, callback = require("clangd_extensions.inlay_hints").disable_inlay_hints })
             vim.api.nvim_create_autocmd({ "TextChanged", "InsertLeave" }, {
