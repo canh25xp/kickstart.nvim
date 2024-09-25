151339304f8217229654d6a3b0f9799286337424
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-09-26 02:16:00 +0700

    conform: disabled format on save

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -21,24 +21,23 @@ return {
   },
   opts = {
     notify_on_error = false,
-
     default_format_opts = {
       timeout_ms = 3000,
       async = false, -- not recommended to change
       quiet = false, -- not recommended to change
       lsp_format = "fallback", -- not recommended to change
     },
-    format_on_save = function(bufnr)
-      -- Disable "format_on_save lsp_fallback" for languages that don't have a well standardized coding style.
-      local disable_filetypes = {
-        c = true,
-        cpp = true,
-      }
-      return {
-        timeout_ms = 500,
-        lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-      }
-    end,
+    -- format_on_save = function(bufnr)
+    --   -- Disable "format_on_save lsp_fallback" for languages that don't have a well standardized coding style.
+    --   local disable_filetypes = {
+    --     c = true,
+    --     cpp = true,
+    --   }
+    --   return {
+    --     timeout_ms = 500,
+    --     lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+    --   }
+    -- end,
     formatters_by_ft = {
       lua = { "stylua" },
       sh = { "shfmt" },
