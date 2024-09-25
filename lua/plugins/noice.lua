4e26ee7ea194aae27d66f47d608acfd94bf4c6d2
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-09-26 02:01:42 +0700

    noice disable lsp overide

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -25,9 +25,16 @@ return {
           enabled = true,
         },
         override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true, -- override the default lsp markdown formatter with Noice
-          ["vim.lsp.util.stylize_markdown"] = true, -- override cmp documentation with Noice (needs the other options to work)
-          ["cmp.entry.get_documentation"] = true, -- override the lsp markdown formatter with Noice
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = false, -- override the default lsp markdown formatter with Noice
+          ["vim.lsp.util.stylize_markdown"] = false, -- override cmp documentation with Noice (needs the other options to work)
+          ["cmp.entry.get_documentation"] = false, -- override the lsp markdown formatter with Noice
+        },
+        hover = {
+          enabled = false,
+          silent = false, -- set to true to not show a message if hover is not available
+          view = nil, -- when nil, use defaults from documentation
+          ---@type NoiceViewOptions
+          opts = {}, -- merged with defaults from documentation
         },
       },
       cmdline = {
@@ -36,7 +43,7 @@ return {
       presets = {
         -- bottom_search = true, -- use a classic bottom cmdline for search
         -- command_palette = true, -- position the cmdline and popupmenu together
-        long_message_to_split = true, -- long messages will be sent to a split
+        -- long_message_to_split = true, -- long messages will be sent to a split
         -- inc_rename = true, -- enables an input dialog for inc-rename.nvim
         -- lsp_doc_border = true, -- add a border to hover docs and signature help
       },
