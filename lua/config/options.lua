a814c7faf1a7bde4b77ebbb1befd53960bab155a
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-09-26 00:10:35 +0700

    bug: lost command line

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -41,6 +41,7 @@ vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.confirm = true -- Ask to save changes before exiting modified buffer
 vim.opt.laststatus = 3 -- global statusline
+-- BUG: Lost command bar when press save (Ctrl - S)
 vim.opt.cmdheight = 0
 vim.opt.mousemoveevent = true
 vim.opt.virtualedit = "block" -- Allow cursor to move where there is no text in visual block mode
