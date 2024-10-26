01997c9dad8a75b2e92916254ca0192cf05666c0
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-10-26 20:44:50 +0700

    suppress warning

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -46,6 +46,7 @@ return {
 
       for name, sign in pairs(require("common.ui").icons.dap) do
         sign = type(sign) == "table" and sign or { sign }
+        ---@diagnostic disable-next-line: assign-type-mismatch
         vim.fn.sign_define("Dap" .. name, { text = sign[1], texthl = sign[2] or "DiagnosticInfo", linehl = sign[3], numhl = sign[3] })
       end
     end,
