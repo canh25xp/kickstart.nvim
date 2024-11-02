1c7df73ef838a990e8208db21a81e291ca358f40
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-11-02 23:38:59 +0700

    move config/init.lua to init.lua

--- init.lua
+++ init.lua
@@ -1,7 +1,16 @@
 vim.opt.runtimepath:prepend("~/.vim/after")
 vim.opt.packpath = vim.opt.runtimepath:get()
-
-if vim.loop.fs_stat("~/.vimrc") then
+if (vim.uv or vim.loop).fs_stat("~/.vimrc") then
   vim.cmd("source ~/.vimrc")
 end
-require("config")
+
+require("config.options")
+require("config.keymaps")
+require("config.autocmds")
+if vim.g.load_lazy_plugins then
+  require("config.lazy")
+end
+
+if vim.g.vscode then
+  require("config.vscode")
+end
