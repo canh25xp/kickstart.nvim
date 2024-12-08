9f6fcf763f7c0a2e3b8784c8d31984eb01636f01
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-12-08 16:53:24 +0700

    add latex specific keymap

--- after/ftplugin/tex.lua
+++ after/ftplugin/tex.lua
@@ -6,5 +6,5 @@ vim.o.wrap = true
 map("v", "<localleader>sm", "<esc>`<i$<esc>`>la$<esc>", { desc = "Surround with Math Mode" })
 map("n", "<localleader>sm", "ciW$<esc>pa$<esc>", { desc = "Surround with Math Mode" })
 
-map("v", "<localleader>st", [[s\text{<esc>pa}<esc>]], { desc = "Surround with text" })
-map("n", "<localleader>st", [[ciW\text{<esc>pa}<esc>]], { desc = "Surround with text" })
+map("v", "<localleader>st", [[s\text{ <esc>pa }<esc>]], { desc = "Surround with text" })
+map("n", "<localleader>st", [[ciW\text{ <esc>pa }<esc>]], { desc = "Surround with text" })
