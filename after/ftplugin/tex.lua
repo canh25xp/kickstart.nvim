a7dd4f189123eb685736491172f2f22a3cb6c534
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-12-08 16:11:51 +0700

    latex ftplugin

--- after/ftplugin/tex.lua
+++ after/ftplugin/tex.lua
@@ -1,2 +1,10 @@
+local map = vim.keymap.set
+
 vim.o.textwidth = 120
 vim.o.wrap = true
+
+map("v", "<localleader>sm", "<esc>`<i$<esc>`>la$<esc>", { desc = "Surround with Math Mode" })
+map("n", "<localleader>sm", "ciW$<esc>pa$<esc>", { desc = "Surround with Math Mode" })
+
+map("v", "<localleader>st", [[s\text{<esc>pa}<esc>]], { desc = "Surround with text" })
+map("n", "<localleader>st", [[ciW\text{<esc>pa}<esc>]], { desc = "Surround with text" })
