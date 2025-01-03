43b6d6ac975c9dd304ef317f36f952429383434c
Ngô Văn Cảnh <canh25xp@gmail.com>
2025-01-03 10:36:54 +0700

    telescope: use cmake instead of make for build

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,10 +6,8 @@ return {
     "nvim-lua/plenary.nvim",
     {
       "nvim-telescope/telescope-fzf-native.nvim",
-      build = "make",
-      cond = function()
-        return vim.fn.executable("make") == 1
-      end,
+      cond = function() return vim.fn.executable("cmake") == 1 end,
+      build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
     },
     { "nvim-telescope/telescope-ui-select.nvim" },
     { "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
