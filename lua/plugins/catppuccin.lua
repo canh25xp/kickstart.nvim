45e04730984ddab5db75431d348717b4366ca243
Ngô Văn Cảnh <canh25xp@gmail.com>
2025-01-11 12:57:27 +0700

    catppuccin: disabled dim_inactive

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -36,7 +36,7 @@ return {
       dark = "mocha",
     },
     dim_inactive = {
-      enabled = true, -- dims the background color of inactive window
+      enabled = false, -- dims the background color of inactive window
       shade = "dark",
       percentage = 0.15, -- percentage of the shade to apply to the inactive window
     },
