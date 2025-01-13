166a523473b93f4bfef39078e45a5f186bfb67a6
Ngô Văn Cảnh <canh25xp@gmail.com>
2025-01-13 19:02:55 +0700

    simplified plugin setup

--- lua/plugins/url-open.lua
+++ lua/plugins/url-open.lua
@@ -3,13 +3,7 @@ return {
   branch = "mini",
   event = "VeryLazy",
   cmd = "URLOpenUnderCursor",
-  config = function()
-    local status_ok, url_open = pcall(require, "url-open")
-    if not status_ok then
-      return
-    end
-    url_open.setup({})
-  end,
+  opts = {},
   keys = {
     { "gx", "<esc>:URLOpenUnderCursor<cr>", desc = "Open URL" },
   },
