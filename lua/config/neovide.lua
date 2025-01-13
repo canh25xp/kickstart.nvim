51f9b912b206edcf50c21952949406fab83c48c3
Ngô Văn Cảnh <canh25xp@gmail.com>
2025-01-13 18:49:50 +0700

    format code

--- lua/config/neovide.lua
+++ lua/config/neovide.lua
@@ -1,5 +1,5 @@
-  vim.g.neovide_fullscreen = false
-  vim.g.neovide_scroll_animation_length = 0.3
-  vim.g.neovide_cursor_vfx_mode = "sonicboom"
-  vim.g.neovide_hide_mouse_when_typing = true
-  vim.g.neovide_transparency = 1.0
+vim.g.neovide_fullscreen = false
+vim.g.neovide_scroll_animation_length = 0.3
+vim.g.neovide_cursor_vfx_mode = "sonicboom"
+vim.g.neovide_hide_mouse_when_typing = true
+vim.g.neovide_transparency = 1.0
