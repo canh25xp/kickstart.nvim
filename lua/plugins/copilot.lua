f8b0892fd20dad35b57eb97af7c8e99576a038fa
Ngô Văn Cảnh <canh25xp@gmail.com>
2025-01-17 01:46:40 +0700

    minor: squash to 1 line

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -4,10 +4,7 @@ return {
   cmd = "Copilot",
   event = "InsertEnter",
   config = function()
-    vim.keymap.set("i", "<C-j>", 'copilot#Accept("\\<CR>")', {
-      expr = true,
-      replace_keycodes = false,
-    })
+    vim.keymap.set("i", "<C-j>", 'copilot#Accept("\\<CR>")', { expr = true, replace_keycodes = false })
     vim.g.copilot_no_tab_map = true
   end,
 }
