49d281cf9d88ada3658cb8d7023a1a9d692a93d4
Ngô Văn Cảnh <canh25xp@gmail.com>
2025-01-17 01:36:04 +0700

    copilot: add keymap and cmd

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,5 +1,13 @@
 return {
   "github/copilot.vim",
   enable = true,
+  cmd = "Copilot",
   event = "InsertEnter",
+  config = function()
+    vim.keymap.set("i", "<C-j>", 'copilot#Accept("\\<CR>")', {
+      expr = true,
+      replace_keycodes = false,
+    })
+    vim.g.copilot_no_tab_map = true
+  end,
 }
