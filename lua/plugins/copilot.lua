c69d6e84b26870bad0381eb8184fcdb15062af85
Ngô Văn Cảnh <canh25xp@gmail.com>
2025-01-17 01:51:04 +0700

    add copilot.lua config for later try

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -8,3 +8,29 @@ return {
     vim.g.copilot_no_tab_map = true
   end,
 }
+
+-- return {
+--     "zbirenbaum/copilot.lua",
+--     cmd = "Copilot",
+--     build = ":Copilot auth",
+--     event = "BufEnter",
+--     config = function()
+--         require("copilot").setup({
+--             suggestion = {
+--                 auto_trigger = true,
+--                 keymap = {
+--                     accept = "<M-l>",
+--                     accept_line = "<M-L>",
+--                     accept_word = "<M-;>",
+--                 },
+--             },
+--             panel = { enabled = false },
+--             filetypes = {
+--                 markdown = true,
+--                 yaml = true,
+--                 help = true,
+--                 ["grug-far"] = false,
+--             },
+--         })
+--     end,
+-- }
