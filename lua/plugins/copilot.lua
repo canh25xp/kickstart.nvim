44dc3e0feefe1b8435aedede7f78486d96c4457a
Ngô Văn Cảnh <canh25xp@gmail.com>
2025-01-18 22:52:14 +0700

    copilot: only enabled for some filetype

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -6,6 +6,13 @@ return {
   config = function()
     vim.keymap.set("i", "<C-j>", 'copilot#Accept("\\<CR>")', { expr = true, replace_keycodes = false })
     vim.g.copilot_no_tab_map = true
+    vim.g.copilot_filetypes = {
+      ["*"] = false,
+      python = true,
+      c = true,
+      cpp = true,
+      lua = true,
+    }
   end,
 }
 
