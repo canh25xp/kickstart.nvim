c4e55363c36a5a23c179a522056b9788a52a15e0
Ngô Văn Cảnh <canh25xp@gmail.com>
2025-01-21 21:57:38 +0700

    separate mason and lsp config file

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -428,23 +428,4 @@ return {
     lazy = true,
     opts = {},
   },
-  {
-    "williamboman/mason.nvim",
-    keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
-    opts = {
-      ui = {
-        border = "rounded",
-        width = 0.9,
-        height = 0.8,
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-        keymaps = {
-          toggle_help = "?",
-        },
-      },
-    },
-  },
 }
