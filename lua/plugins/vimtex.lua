308b064aa170c9a285eeebd87eaab464581b8b36
Ngô Văn Cảnh <canh25xp@gmail.com>
2025-01-27 03:00:44 +0700

    vimtex: use stable version

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -1,6 +1,7 @@
 return {
   "lervag/vimtex",
   lazy = false, -- lazy-loading will disable inverse search
+  version = "*",
   config = function()
     vim.g.vimtex_mappings_disable = { ["n"] = { "K" } } -- disable `K` as it conflicts with LSP hover
     vim.g.vimtex_quickfix_method = vim.fn.executable("pplatex") == 1 and "pplatex" or "latexlog"
