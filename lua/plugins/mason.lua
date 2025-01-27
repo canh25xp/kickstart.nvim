793024085708890cd7e6abb419904ac790897690
Ngô Văn Cảnh <canh25xp@gmail.com>
2025-01-28 01:54:05 +0700

    mason: turn off auto update

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -3,6 +3,7 @@ return {
   keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
   opts = {
     ui = {
+      check_outdated_packages_on_open = false,
       border = "rounded",
       width = 0.9,
       height = 0.8,
