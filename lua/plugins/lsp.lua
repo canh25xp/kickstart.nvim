8084d9d8afd87a0fa4823357451a319804fc474a
Ngô Văn Cảnh <canh25xp@gmail.com>
2025-01-28 20:41:07 +0700

    format code

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -109,18 +109,14 @@ return {
           require("clangd_extensions.inlay_hints").set_inlay_hints()
           local group = vim.api.nvim_create_augroup("clangd_no_inlay_hints_in_insert", { clear = true })
 
-          vim.keymap.set("n", "<leader>uh", function()
+          map("n", "<leader>uh", function()
             if require("clangd_extensions.inlay_hints").toggle_inlay_hints() then
               vim.api.nvim_create_autocmd("InsertEnter", { group = group, buffer = bufnr, callback = require("clangd_extensions.inlay_hints").disable_inlay_hints })
-              vim.api.nvim_create_autocmd({ "TextChanged", "InsertLeave" }, {
-                group = group,
-                buffer = bufnr,
-                callback = require("clangd_extensions.inlay_hints").set_inlay_hints,
-              })
+              vim.api.nvim_create_autocmd({ "TextChanged", "InsertLeave" }, { group = group, buffer = bufnr, callback = require("clangd_extensions.inlay_hints").set_inlay_hints })
             else
               vim.api.nvim_clear_autocmds({ group = group, buffer = bufnr })
             end
-          end, { buffer = bufnr, desc = "Clang Inlay Hints toggle" })
+          end, "Clang Inlay Hints toggle")
         end
       end
 
