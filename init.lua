04bbfc5483b9e17975215ab861a6c200eda4073c
Ngô Văn Cảnh <canh25xp@gmail.com>
2025-02-12 10:33:09 +0700

    add is_android global variable

--- init.lua
+++ init.lua
@@ -12,6 +12,7 @@ vim.g.load_lazy_plugins = true
 vim.g.dynamic_cmdheight = true
 vim.g.is_windows = vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1
 vim.g.is_linux = vim.fn.has("unix") == 1
+vim.g.is_android = vim.fn.has("android") == 1
 vim.g.is_wsl = vim.fn.has("wsl") == 1
 vim.g.is_mac = vim.fn.has("mac") == 1
 vim.g.path_sep = vim.g.is_windows and ";" or ":"
