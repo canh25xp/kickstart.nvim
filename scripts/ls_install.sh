5856e29f6ec9ce1319746eed76d071412bdf6f4c
Ngô Văn Cảnh <canh25xp@gmail.com>
2025-02-13 14:13:30 +0700

    ls install scrip: add npm package

--- scripts/ls_install.sh
+++ scripts/ls_install.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
 pkg install clang lua-language-server stylua ruff texlab shfmt taplo
+
+npm install -g prettierd
+npm install -g pyright
+npm install -g vscode-langservers-extracted
+npm install -g markdownlint-cli
--- scripts/npm_install.ps1
+++ scripts/npm_install.ps1
@@ -1,3 +1,4 @@
 npm install -g prettierd
 npm install -g pyright
 npm install -g vscode-langservers-extracted
+npm install -g markdownlint-cli
