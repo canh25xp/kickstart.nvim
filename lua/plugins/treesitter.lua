b4a211be256cfd9cef6cb582441d39539a5c256b
Ngô Văn Cảnh <canh25xp@gmail.com>
2025-02-13 16:08:22 +0700

    treesitter: disable for tmux

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -34,7 +34,7 @@ return {
       -- add the language to the list of additional_vim_regex_highlighting and disabled languages for indent.
       additional_vim_regex_highlighting = { "ruby" },
       disable = function()
-        if string.find(vim.bo.filetype, "chezmoitmpl") or vim.bo.filetype == "tex" then
+        if string.find(vim.bo.filetype, "chezmoitmpl") or vim.bo.filetype == "tex" or vim.bo.filetype == "tmux" then
           return true
         end
       end,
