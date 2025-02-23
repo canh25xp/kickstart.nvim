c7e975536dbf7d4272e1fa35636817c4897a2e26
Ngô Văn Cảnh <nv.canh@outlook.com>
2025-02-23 11:28:18 +0700

    edit clangd install script

--- scripts/clangd_install.ps1
+++ scripts/clangd_install.ps1
@@ -1,10 +1,16 @@
 $tools_dir = "$env:LOCALAPPDATA\nvim-data\tools"
-$download_link = "https://github.com/clangd/clangd/releases/download/18.1.3/clangd-windows-18.1.3.zip"
+$download_link = "https://github.com/clangd/clangd/releases/download/19.1.2/clangd-windows-19.1.2.zip"
 $download_path = "$tools_dir\clangd.zip"
 $install_dir = "$tools_dir\clangd"
 
 curl -L $download_link -o $download_path
 
-7z x "$download_path" -o"$install_dir"
+7z x "$download_path" -oclangd_tmp
+
+mkdir $install_dir
+
+Move-Item clangd_tmp/* $install_dir
+
+Remove-Item clangd_tmp -Recurse
 
 Remove-Item $download_path
