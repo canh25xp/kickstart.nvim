c44f293bb7d1a934ab58fd1fc94eea36fb05ed8e
Ngô Văn Cảnh <nv.canh@outlook.com>
2025-02-23 12:49:37 +0700

    edit powershell_es install script

--- scripts/powershell_es_install.ps1
+++ scripts/powershell_es_install.ps1
@@ -1,10 +1,6 @@
-$tools_dir = "$env:LOCALAPPDATA\nvim-data\tools"
-$download_link = "https://github.com/PowerShell/PowerShellEditorServices/releases/latest/download/PowerShellEditorServices.zip"
-$download_path = "$tools_dir\PowerShellEditorServices.zip"
-$install_dir = "$tools_dir\powershell-editor-services"
-
-curl -L $download_link -o $download_path
-
-7z x "$download_path" -o"$install_dir"
-
-Remove-Item $download_path
+$DownloadUrl = 'https://github.com/PowerShell/PowerShellEditorServices/releases/latest/download/PowerShellEditorServices.zip';
+$ZipPath = "$HOME/PowerShellEditorServices.zip";
+$InstallPath = "$HOME/.local/PowerShellEditorServices";
+Invoke-WebRequest -Method 'GET' -Uri $DownloadUrl -OutFile $ZipPath;
+Expand-Archive -Path $ZipPath -DestinationPath $InstallPath;
+Remove-Item $ZipPath
