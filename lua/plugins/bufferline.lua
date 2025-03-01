1376bc2e622f8cff016443a5efe791b637df5381
Ngô Văn Cảnh <nv.canh@outlook.com>
2025-03-01 20:11:24 +0700

    bufferline: use opts

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -15,31 +15,27 @@ return {
     { "[B", "<cmd>BufferLineMovePrev<cr>", desc = "Move buffer prev" },
     { "]B", "<cmd>BufferLineMoveNext<cr>", desc = "Move buffer next" },
   },
-  config = function()
-    local bufferline = require("bufferline")
-    bufferline.setup({
-      options = {
-        hover = {
-          enabled = true,
-          delay = 200,
-          reveal = { "close" },
-        },
-        style_preset = {
-          bufferline.style_preset.no_italic,
-          bufferline.style_preset.no_bold,
-        },
-        show_tab_indicators = true,
-        always_show_bufferline = false,
-        offsets = {
-          {
-            filetype = "neo-tree",
-            text = "Neo-tree",
-            highlight = "Directory",
-            text_align = "left",
-          },
+  opts = {
+    options = {
+      hover = {
+        enabled = true,
+        delay = 200,
+        reveal = { "close" },
+      },
+      show_tab_indicators = true,
+      always_show_bufferline = false,
+      offsets = {
+        {
+          filetype = "neo-tree",
+          text = "Neo-tree",
+          highlight = "Directory",
+          text_align = "left",
         },
       },
-    })
+    },
+  },
+  config = function(_, opts)
+    require("bufferline").setup(opts)
     -- Fix bufferline when restoring a session
     vim.api.nvim_create_autocmd({ "BufAdd", "BufDelete" }, {
       callback = function()
