d2b811827dedbde566d388c8ada0f7f5dcb05fe3
Ngô Văn Cảnh <nv.canh@outlook.com>
2025-03-01 21:31:53 +0700

    edit npm install script: prettierd

--- scripts/ls_install.sh
+++ scripts/ls_install.sh
@@ -2,7 +2,7 @@
 
 pkg install clang lua-language-server stylua ruff texlab shfmt taplo
 
-npm install -g prettierd
+npm install -g @fsouza/prettierd
 npm install -g pyright
 npm install -g vscode-langservers-extracted
 npm install -g markdownlint-cli
--- scripts/npm_install.ps1
+++ scripts/npm_install.ps1
@@ -1,4 +1,4 @@
-npm install -g prettierd
+npm install -g @fsouza/prettierd
 npm install -g pyright
 npm install -g vscode-langservers-extracted
 npm install -g markdownlint-cli
