b1bbb1943bf658100a11328111386aff2d63d4d6
Ngô Văn Cảnh <nv.canh@outlook.com>
2025-03-09 21:14:05 +0700

    lsp: change clangd cmd

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -81,7 +81,7 @@ return {
           flags = {
             debounce_text_changes = 500,
           },
-          cmd = { "clangd", "--background-index", "--clang-tidy", "--log=verbose" },
+          cmd = { "clangd", "--background-index", "--clang-tidy", "--completion-style=detailed" },
           init_options = {
             fallback_flags = { "-std=c++17" },
           },
