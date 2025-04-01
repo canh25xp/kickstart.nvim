6fca5261d561a04f4ef6f3678ed17ca3eb4ed0ec
Ngô Văn Cảnh <nv.canh@outlook.com>
2025-04-01 21:50:18 +0700

    bufferline: remove redundant keymaps in v0.11

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -10,10 +10,6 @@ return {
     { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete Buffers to the Left" },
     { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
     { "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
-    { "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
-    { "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
-    { "[B", "<cmd>BufferLineMovePrev<cr>", desc = "Move buffer prev" },
-    { "]B", "<cmd>BufferLineMoveNext<cr>", desc = "Move buffer next" },
   },
   opts = {
     options = {
